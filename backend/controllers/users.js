09636a4d6f91cf27c06c74f176141ae53a57a578
Fernandes <vs.masich@gmail.com>
2021-01-11 23:30:39 +0300

    fixes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -88,11 +88,11 @@ function login(req, res, next) {
         NODE_ENV === 'production' ? JWT_SECRET : 'vodka-bear-balalayka',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 7 * 24,
-        httpOnly: true,
-      });
-      res.status(200).send({ message: 'cookie!' });
+      // res.cookie('jwt', token, {
+      //   maxAge: 3600000 * 7 * 24,
+      //   httpOnly: true,
+      // });
+      res.status(200).send({ token });
     })
     .catch(next);
 }
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://zandalar.students.nomoreparties.xyz';
 
 export function checkAnswer(res) {
   if (res.ok) {
