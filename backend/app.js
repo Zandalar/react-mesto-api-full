98cae80e056cefb8183316b4871c0d2b0b8401b1
Fernandes <vs.masich@gmail.com>
2021-01-20 19:48:52 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -22,29 +22,29 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use(cors({ origin: true }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const allowedCors = [
-//   'https://masich.students.nomoredomains.rocks',
-//   'http://masich.students.nomoredomains.rocks',
-//   'https://api.masich.students.nomoredomains.rocks',
-//   'http://api.masich.students.nomoredomains.rocks',
-//   'localhost:3001',
-// ];
+const allowedCors = [
+  'https://masich.students.nomoredomains.rocks',
+  'http://masich.students.nomoredomains.rocks',
+  'https://api.masich.students.nomoredomains.rocks',
+  'http://api.masich.students.nomoredomains.rocks',
+  'localhost:3001',
+];
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
-//
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
-//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  }
+  next();
+});
 
 app.use(requestLogger);
 
