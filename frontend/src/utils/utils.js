3a954135d3a362f0e1b0100c4dcd12cb43b369b9
Fernandes <vs.masich@gmail.com>
2021-01-21 20:40:11 +0300

    fix

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,4 @@
-export const BASE_URL = 'https://api.fernandes.students.nomoredomains.rocks';
-// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.masich.students.nomoredomains.rocks';
 
 export function checkResponse(res) {
 	if (res.ok) {
