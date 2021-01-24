77960b3402c05cbb79a336d68d03d0467d879636
Fernandes <vs.masich@gmail.com>
2021-01-24 15:30:42 +0300

    fix cards and users controllers

--- backend/routes/cardsRouter.js
+++ backend/routes/cardsRouter.js
@@ -9,14 +9,30 @@ const {
 } = require('../controllers/cards');
 
 cardsRouter.get('/cards', getCards);
+
 cardsRouter.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/),
   }),
 }), createCard);
-cardsRouter.delete('/cards/:cardId', deleteCard);
-cardsRouter.put('/cards/:cardId/likes', likeCard);
-cardsRouter.delete('/cards/:cardId/likes', dislikeCard);
+
+cardsRouter.delete('/cards/:cardId', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
+
+cardsRouter.put('/cards/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard);
+
+cardsRouter.delete('/cards/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = cardsRouter;
--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -8,13 +8,20 @@ const {
 } = require('../controllers/users');
 
 usersRouter.get('/users', getUsers);
-usersRouter.get('/users/:id', getUserById);
+
+usersRouter.get('/users/:id', celebrate({
+  body: Joi.object().keys({
+    _id: Joi.string().hex().length(24),
+  }),
+}), getUserById);
+
 usersRouter.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
+
 usersRouter.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/),
